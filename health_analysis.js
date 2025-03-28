82e65fbf78ebfc3d2f11e3963237cdedfb011669
ingarskrafts <ingarskrafts@gmail.com>
2025-03-28 07:01:57 -0400

    bugfix

--- health_analysis.js
+++ health_analysis.js
@@ -16,43 +16,7 @@ function addPatient() {
     }
 }
 
-function resetForm() {
-    document.getElementById("name").value = "";
-    document.querySelector('input[name="gender"]:checked').checked = false;
-    document.getElementById("age").value = "";
-    document.getElementById("condition").value = "";
-}
 
-function searchCondition() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
-
-    fetch('health_analysis.json')
-        .then(response => response.json())
-        .then(data => {
-            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
-
-            if (condition) {
-                const symptoms = condition.symptoms.join(', ');
-                const prevention = condition.prevention.join(', ');
-                const tratment = condition.tratment;
-
-                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-                resultDiv.innerHTML +=`<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-                resultDiv.innerHTML +=`<p><strong>Prevention:</strong> ${prevention}</p>`;
-                resultDiv.innerHTML +=`<p><strong>Treatment:</strong> ${tratment}</p>`;
-            } else {
-                resultDiv.innerHTML = 'Condition not found';
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = 'An error occurred while fetching data';
-        });
-}
 
 function generateReport() {
     const numPatients = patients.length;
@@ -94,4 +58,43 @@ function generateReport() {
     }
 }
 
+function resetForm() {
+    document.getElementById("name").value = "";
+    document.querySelector('input[name="gender"]:checked').checked = false;
+    document.getElementById("age").value = "";
+    document.getElementById("condition").value = "";
+}
+
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+        .then(response => response.json())
+        .then(data => {
+            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+            if (condition) {
+                const symptoms = condition.symptoms.join(', ');
+                const prevention = condition.prevention.join(', ');
+                const tratment = condition.treatment;
+
+                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="picture not found" onerror="this.src='medical.jpg'">`;
+
+                resultDiv.innerHTML +=`<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+                resultDiv.innerHTML +=`<p><strong>Prevention:</strong> ${prevention}</p>`;
+                resultDiv.innerHTML +=`<p><strong>Treatment:</strong> ${tratment}</p>`;
+            } else {
+                resultDiv.innerHTML = 'Condition not found';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data';
+        });
+}
+
+btnSearch.addEventListener("click", searchCondition);
 addPatientButton.addEventListener("click", addPatient);
